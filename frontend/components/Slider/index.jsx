38131a04a8f810fbc9d1d37080949fa58e53a223
Philipp Mank <philipp.mank@shopgate.com>
2019-04-23 13:06:34 +0200

    changed logic

--- frontend/components/Slider/index.jsx
+++ frontend/components/Slider/index.jsx
@@ -5,68 +5,91 @@ import Slider from '@shopgate/pwa-common/components/Slider';
 import styles from './style';
 
 /**
+* Creates an Item for a Slide.
+* @param {Object} slideItem The Slide Item data.
+* @param {number} index The Slide Item data.
+* @return {JSX} The Slide Item.
+*/
+const createSlideItem = (slideItem, index) => {
+  if (typeof slideItem.img !== 'string') {
+    return (
+      <img className={styles.item} key={`${slideItem}-${index}`} alt="" src={slideItem} />
+    );
+  }
+  if (typeof slideItem.link !== 'string' || slideItem.link.length === 0) {
+    return (
+      <img className={styles.item} key={`${slideItem.img}-${index}`} alt="" src={slideItem.img} />
+    );
+  }
+
+  return (
+    <Link
+      href={slideItem.link}
+      key={slideItem.link + index}
+    >
+      <img className={styles.item} key={`${slideItem.img}-${index}`} alt="" src={slideItem.img} />
+    </Link>
+  );
+};
+
+/**
+* Creates an Slide with Items.
+* @param {Object} slide The Slide data.
+* @param {number} index The Slide Item data.
+* @return {JSX} The rendered Slide.
+*/
+const createSlide = (slide, index) => (
+  <Slider.Item key={index}>
+    <div className={styles.sliderWrapper}>
+      {slide.map((
+        (slideItem, slideItemIndex) => createSlideItem(slideItem, slideItemIndex)
+      ))}
+    </div>
+  </Slider.Item>
+);
+
+/**
  * Renders the component.
  * @param {Object} props The component props.
  * @returns {JSX}
  */
 const SliderWidget = (props) => {
-  /**
-  * Creates an Item for a Slide.
-  * @param {Object} slideItem The Slide Item data.
-  * @return {JSX} The Slide Item.
-  */
-  const createSliderItem = (slideItem) => {
-    if (slideItem.img === '' || slideItem.img === undefined) {
-      return (
-        <img key="" className={styles.item} alt="" src={slideItem} />
-      );
-    }
+  if (!props.settings.slides) {
+    return null;
+  }
 
-    if (slideItem.link === '' || slideItem.link === undefined) {
-      return (
-        <img key="" className={styles.item} alt="" src={slideItem.img} />
-      );
-    }
-
-    return (
-      <Link
-        href={slideItem.link}
-        key=""
-      >
-        <img key="" className={styles.item} alt="" src={slideItem.img} />
-      </Link>
-    );
+  const mergedSettings = {
+    slides: null,
+    autoplay: true,
+    loop: true,
+    ...props.settings,
   };
 
-  /**
-  * Creates an Slide with Items.
-  * @param {Object} slide The Slide data.
-  * @return {JSX} The rendered Slide.
-  */
-  const createSlide = slide => (
-    <div key="">
-      <Slider.Item>
-        <div className={styles.sliderWrapper}>
-          {slide.map((
-            slideItem => createSliderItem(slideItem)
-          ))}
-        </div>
-      </Slider.Item>
-    </div>
-  );
-
-  const slides = props.settings.slides.map((
-    slide => createSlide(slide)
+  const slides = mergedSettings.slides.map((
+    (slide, slideIndex) => createSlide(slide, slideIndex)
   ));
+
+  if (!mergedSettings.headline || mergedSettings.headline === '') {
+    return (
+      <div>
+        <Slider
+          autoPlay={mergedSettings.autoplay}
+          loop={mergedSettings.loop}
+        >
+          {slides}
+        </Slider>
+      </div>
+    );
+  }
 
   return (
     <div>
       <div className={styles.headlineContainer}>
-        <h3 className={styles.headline}>{props.settings.headline ? props.settings.headline : ''}</h3>
+        <h3 className={styles.headline}>{mergedSettings.headline}</h3>
       </div>
       <Slider
-        autoPlay
-        loop
+        autoPlay={mergedSettings.autoplay}
+        loop={mergedSettings.loop}
       >
         {slides}
       </Slider>
@@ -76,13 +99,19 @@ const SliderWidget = (props) => {
 
 SliderWidget.propTypes = {
   settings: PropTypes.shape({
-    slides: PropTypes.array,
+    slides: PropTypes.array.isRequired,
     headline: PropTypes.string,
+    autoplay: PropTypes.bool,
+    loop: PropTypes.bool,
   }),
 };
 
 SliderWidget.defaultProps = {
-  settings: null,
+  settings: {
+    slides: null,
+    autoplay: true,
+    loop: true,
+  },
 };
 
 export default SliderWidget;
